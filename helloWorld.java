dbe57f74863dc012c0d4f4ac18731886919383c2
unknown <agnasa7@gmail.com>
2015-04-06 22:15:40 +0700

    first commit

---
+++ helloWorld.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author agnasa
+ */
+public class Main {
+
+    public static void main(String[] args) {
+		System.out.println("Hello, Saya Agna. NIM 63906131084");
+		System.out.println("Saya sangat menyukai praktikum RPL Modul 8 ini ");
+    }
+	
+}
